241ddf5cbb2aa909516dc947eb7018c43bc224b6
StanleySB <game.stem@rambler.ru>
2020-10-27 12:55:11 +0200

    Updates

---
+++ precache-manifest.be697e8aa9cc89483ae8a48c0592eff9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9458e3ab6c3761d1d0025b808905cd0f",
+    "url": "/Pokemon-app-react-hooks-/index.html"
+  },
+  {
+    "revision": "24a284cccdc1eb137397",
+    "url": "/Pokemon-app-react-hooks-/static/js/2.a193f524.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Pokemon-app-react-hooks-/static/js/2.a193f524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f2b3dd89b69dadb5cf2",
+    "url": "/Pokemon-app-react-hooks-/static/js/main.ff4079c6.chunk.js"
+  },
+  {
+    "revision": "04774435996d9deb254c",
+    "url": "/Pokemon-app-react-hooks-/static/js/runtime-main.bebd4d2d.js"
+  }
+]);
